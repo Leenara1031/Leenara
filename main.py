4a50752c615acd37de53baf77b28d396542e7da8
Leenara1031 <93296457+Leenara1031@users.noreply.github.com>
2021-11-02 14:24:35 +0900

    textAnalysis

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
-import json
-import urllib.parse
-import datetime
+import pandas as pd
+import glob
+import re
+from functools import reduce
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from collections import  Counter
+import matplotlib.pyplot as plt
+from wordcloud import STOPWORDS, WordCloud
 
-def main():
-    node = 'news'
-    srcText = input('검색어를 입력하세요: ')
-    cnt = 0
-    jsonResult = []
+nltk.download()
 
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)
-    total = jsonResponse['total']
+all_files = glob.glob('./textAnalysis/baseResources/myCabinetExcelData*.xls')
+print(all_files)
 
-    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
-        for post in jsonResponse['items']:
-            cnt += 1
-            getPostData(post, jsonResult, cnt)
+all_files_data = []
+for file in all_files:
+    data_frame = pd.read_excel(file)
+    all_files_data.append(data_frame)
+all_files_data[0]
 
-            start = jsonResponse['start'] + jsonResponse['display']
-            jsonResponse = getNaverSearch(node, srcText, start, 100)
+all_files_data_concat = pd.concat(all_files_data, axis=0, lgnore_index=True)
+print(all_files_data_concat)
 
-    print('전체 검색 : %d 건' %total)
-
-    with open('%s_naver_%s.json' % (srcText, node), 'W', encoding = 'utf8') as outfile:
-        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True, ensure_ascii = False)
-        outfile.write(jsonFile)
-
-    print("가져온 데이터 : %d 건" %(cnt))
-    print('%s_naver_%s.json SAVED' % (srcText, node))
