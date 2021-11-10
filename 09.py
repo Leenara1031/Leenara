6a3622baff2873fb1379c11112fc49bffa4fe179
Leenara1031 <93296457+Leenara1031@users.noreply.github.com>
2021-11-10 14:43:44 +0900

    Delete 09.py

--- 09.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import networkx as nx
-
-K = np.loadtxt("social.csv", delimiter=",")
-G = nx.to_networkx_graph(K)
-ksub = nx.connected_component_subgraphs(G)
-klist = list(ksub)
-
-nx.draw(klist[0], with_labels=True, node_color='yellow', edge_color='red')
