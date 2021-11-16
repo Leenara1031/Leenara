35b7888b863155e20b4f700f7c3ad334eefbd6b0
Leenara1031 <93296457+Leenara1031@users.noreply.github.com>
2021-11-16 16:03:08 +0900

    Delete Numpy directory

--- Numpy/num13.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
-
-K = np.loadtxt('C:/Users/202-19/AI/test.csv', delimiter=',')
-G = nx.to_networkx_graph(K)
-nx.draw(G)
-plt.show()
-
--- Numpy/num14.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
-
-G = nx.karate_club_graph()
-plt.xlabel('Degree')
-plt.ylabel('Prob. of node degree')
-plt.hist(list([d for n, d in G.degree()]), histtype='step')
